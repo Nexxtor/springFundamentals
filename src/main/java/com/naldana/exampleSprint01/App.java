4234a8dc4947b8a72099d0678f05dc6282cadff0
Nestor Aldana <nestor.aldana1@gmail.com>
2021-08-26 10:45:30 -0600

    Step 1: config basic Spring Container

---
+++ src/main/java/com/naldana/exampleSprint01/App.java
@@ -0,0 +1,17 @@
+package com.naldana.exampleSprint01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		MyService service = ctx.getBean(MyService.class);
+		MyService service1 = ctx.getBean(MyService.class);
+		MyService service2 = ctx.getBean(MyService.class);
+		
+		service.doSomething();
+	}
+}
---
+++ src/main/java/com/naldana/exampleSprint01/MyService.java
@@ -0,0 +1,8 @@
+package com.naldana.exampleSprint01;
+
+public class MyService {
+	
+	public void doSomething() {
+		System.out.println("Doing something important!");
+	}
+}
