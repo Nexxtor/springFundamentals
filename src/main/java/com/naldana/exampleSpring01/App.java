0e04e7a8e6327e574edc098906a5d03959f8ea71
Nestor Aldana <nestor.aldana1@gmail.com>
2021-08-27 08:16:29 -0600

    Step 7: Change application context type

--- src/main/java/com/naldana/exampleSpring01/App.java
+++ src/main/java/com/naldana/exampleSpring01/App.java
@@ -1,14 +1,13 @@
 package com.naldana.exampleSpring01;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class App {
 	
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext ctx = new AnnotationConfigApplicationContext("com.naldana.exampleSpring01");
 		MyService service = ctx.getBean(MyService.class);
 		
 		service.doBusinessLogic();
