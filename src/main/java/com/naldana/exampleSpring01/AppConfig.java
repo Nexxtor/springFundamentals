7f269fdb89db9c20c0e703604ba35e4bffa1e868
Nestor Aldana <nestor.aldana1@gmail.com>
2021-08-27 08:34:32 -0600

    Step 10: Configure Scope of a bean

--- src/main/java/com/naldana/exampleSpring01/AppConfig.java
+++ src/main/java/com/naldana/exampleSpring01/AppConfig.java
@@ -3,11 +3,13 @@ package com.naldana.exampleSpring01;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
+	@Scope(value = "prototype")
 	public MyService getService() {
 		return new MyService(getRepository());
 	}
