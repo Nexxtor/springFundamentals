8f974df0dd337e988c4145c89d171a2f89e6b4dc
Nestor Aldana <nestor.aldana1@gmail.com>
2021-08-27 09:50:29 -0600

    Step 13: Add support to read properties

--- src/main/java/com/naldana/exampleSpring01/AppConfig.java
+++ src/main/java/com/naldana/exampleSpring01/AppConfig.java
@@ -2,9 +2,11 @@ package com.naldana.exampleSpring01;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "com.naldana.exampleSpring01")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
 }
--- src/main/java/com/naldana/exampleSpring01/service/MyServiceImpl.java
+++ src/main/java/com/naldana/exampleSpring01/service/MyServiceImpl.java
@@ -2,6 +2,7 @@ package com.naldana.exampleSpring01.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.naldana.exampleSpring01.data.MyRepository;
@@ -9,6 +10,8 @@ import com.naldana.exampleSpring01.data.MyRepository;
 @Service
 public class MyServiceImpl implements MyService {
 	
+	@Value("${app.name}")
+	private String name;
 
 	private MyRepository repository;
 	
@@ -19,7 +22,7 @@ public class MyServiceImpl implements MyService {
 
 	@Override
 	public void doBusinessLogic() {
-		System.out.println("Doing some business logic!");
+		System.out.println("Doing some business logic for " + name);
 		repository.doQuery();
 	}
 
