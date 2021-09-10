6a017901a71c05180121c42957404c0b1df2b362
oscardonas <oscar.donas@generaldeequipos.com>
2021-09-10 14:04:14 -0600

    He añadido una nueva implementación de MyService llamada MyServiceTest

---
+++ src/main/java/com/naldana/exampleSpring01/service/MyServiceTest.java
@@ -0,0 +1,17 @@
+package com.naldana.exampleSpring01.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyServiceTest implements MyService {
+
+	@Override
+	public void doBusinessLogic() {
+		// TODO Auto-generated method stub
+		System.out.println("Salida de Servicio MyServiceTest Oscar Doñas");
+		
+	}
+	
+	
+
+}
